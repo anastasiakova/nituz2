d0a79e273687b3ab94cd8980c9f80bf2471d5c41
anastasiakova <kovalkov@post.bgu.ac.il>
2019-06-06 18:04:09 +0300

    hop hop we have db

---
+++ code/src/main/java/Model/Main.java
@@ -0,0 +1,211 @@
+package Model;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        initDB();
+    }
+
+    static String _path;
+
+    public static void initDB() {
+        Path currentPath = Paths.get("");
+        TblFields.init();
+        _path = "jdbc:sqlite:" + currentPath.toAbsolutePath().toString() + "\\dataBase.db";
+        try (Connection conn = DriverManager.getConnection(_path)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        //TO DO
+        //should be for each type of table his init function!
+        initUserTbl();
+        initOrganizationTbl();
+        initEventTbl();
+        initEventAndParticipateTbl();
+        initFeedbackTbl();
+        initUpdateTbl();
+        initUpdateVersionTbl();
+        initComplaintTbl();
+        initCategoriesTbl();
+        initEventAndCategoryTbl();
+        initAccountMassagesTbl();
+    }
+
+    public static void initUserTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS user (\n" +
+                TblFields.enumDict.get("user").get(0) + " text NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("user").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("user").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("user").get(3) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("user").get(4) + " INTEGER,\n" +
+                TblFields.enumDict.get("user").get(5) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("user").get(6) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("user").get(7) + " INTEGER NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initOrganizationTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS organization (\n" +
+                TblFields.enumDict.get("organization").get(0) + " INTEGER NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("organization").get(1) + " text NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initEventTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS event (\n" +
+                TblFields.enumDict.get("event").get(0) + " INTEGER NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("event").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("event").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("event").get(3) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("event").get(4) + " text,\n" +
+                TblFields.enumDict.get("event").get(5) + " text,\n" +
+                TblFields.enumDict.get("event").get(6) + " text\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initEventAndParticipateTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS eventAndParticipate (\n" +
+                TblFields.enumDict.get("eventAndParticipate").get(0) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("eventAndParticipate").get(1) + " text NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initFeedbackTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS feedback (\n" +
+                TblFields.enumDict.get("feedback").get(0) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("feedback").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("feedback").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("feedback").get(3) + " INTEGER NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initUpdateTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS updates (\n" +
+                TblFields.enumDict.get("updates").get(0) + " INTEGER NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("updates").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("updates").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("updates").get(3) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("updates").get(4) + " INTEGER\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initUpdateVersionTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS updateVersion (\n" +
+                TblFields.enumDict.get("updateVersion").get(0) + " INTEGER NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("updateVersion").get(1) + " INTEGER NOT NULL,\n" +
+                TblFields.enumDict.get("updateVersion").get(2) + " text NOT NULL\n" +
+                ");";
+
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initComplaintTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS complaint (\n" +
+                TblFields.enumDict.get("complaint").get(0) + " INTEGER NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("complaint").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("complaint").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("complaint").get(3) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("complaint").get(4) + " INTEGER NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initCategoriesTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS categories (\n" +
+                TblFields.enumDict.get("categories").get(0) + " text NOT NULL PRIMARY KEY\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initEventAndCategoryTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS eventAndCategory (\n" +
+                TblFields.enumDict.get("eventAndCategory").get(0) + " text NOT NULL PRIMARY KEY,\n" +
+                TblFields.enumDict.get("eventAndCategory").get(1) + " INTEGER NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void initAccountMassagesTbl(){
+        String createStatement = "CREATE TABLE IF NOT EXISTS accountMassages (\n" +
+                TblFields.enumDict.get("accountMassages").get(0) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("accountMassages").get(1) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("accountMassages").get(2) + " text NOT NULL,\n" +
+                TblFields.enumDict.get("accountMassages").get(3) + " text NOT NULL\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(_path);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ code/src/main/java/Model/TblFields.java
@@ -0,0 +1,46 @@
+package Model;
+
+import java.util.*;
+
+public class TblFields {
+
+    public static Map<String, List<String>> enumDict = new HashMap<String, List<String>>();
+
+    public static void init() {
+        List<String> enumList = new ArrayList<String>();
+        enumList = Arrays.asList("username", "psw", "name", "organization",
+                "rank", "accountStatus", "email", "warnings");
+        enumDict.put("user", enumList);
+
+        enumList = Arrays.asList("organizationName", "adminUsername");
+        enumDict.put("organization", enumList);
+
+        enumList = Arrays.asList("eventId", "title", "eventDate", "eventStatus",
+                "police", "mada", "fire");
+        enumDict.put("event", enumList);
+
+        enumList = Arrays.asList("participateEventId", "participateUsername");
+        enumDict.put("eventAndParticipate", enumList);
+
+        enumList = Arrays.asList("feedbackEventId", "feedbacker", "feedbackee", "feedback");
+        enumDict.put("feedback", enumList);
+
+        enumList = Arrays.asList("updateId", "updateDate", "updater", "updateEventId", "lastUpdateId");
+        enumDict.put("updates", enumList);
+
+        enumList = Arrays.asList("updateVersionId", "version", "description");
+        enumDict.put("updateVersion", enumList);
+
+        enumList = Arrays.asList("complaintId", "complainer", "complainee", "description", "status");
+        enumDict.put("complaint", enumList);
+
+        enumList = Arrays.asList("categoryName");
+        enumDict.put("categories", enumList);
+
+        enumList = Arrays.asList("eventCategoryName", "ecEventId");
+        enumDict.put("eventAndCategory", enumList);
+
+        enumList = Arrays.asList("massageName", "accountUsername", "isNew", "massage");
+        enumDict.put("accountMassages", enumList);
+    }
+}
