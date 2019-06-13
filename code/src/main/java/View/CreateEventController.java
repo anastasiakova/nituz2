1cc57740569deb03450ece9688f77f2789e87fb5
shororen <shororen@post.bgu.ac.il>
2019-06-13 16:10:58 +0300

    user constructors

--- code/src/main/java/View/CreateEventController.java
+++ code/src/main/java/View/CreateEventController.java
@@ -3,12 +3,14 @@ package View;
 import Model.Category;
 import Model.EOCUser;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.controlsfx.control.CheckComboBox;
+
 
 import java.text.ParseException;
 import java.util.Calendar;
@@ -24,9 +26,7 @@ public class CreateEventController {
     public TextArea initialUpdate;
     public Button createEvent;
     public EOCUser publisher; //understand from where to get it
-    public Category [] categories;
-
-
+    public Category[] categories;
 
 
     public CreateEventController() {
@@ -34,22 +34,25 @@ public class CreateEventController {
 
     public void createEventAction() throws ParseException {
         Alert alert = new Alert(Alert.AlertType.WARNING);
-        String headLine = headline.getText();
-        String initUpdate = initialUpdate.getText();
-        String policeName = policePart.getText();
-        String EMSName = EMSPart.getText();
-        String FDName = FDPart.getText();
-        Calendar calendar = Calendar.getInstance();
-        Date publishTime = calendar.getTime();
+        if(headline.getText().equals("") || headline == null){
+            alert.setContentText("Please enter headline for this event");
+            alert.show();
+        }else if(initialUpdate.getText().equals("") || initialUpdate == null){
+            alert.setContentText("Please enter your first update");
+            alert.show();
+        }else if((policePart.getText().equals("") && EMSPart.getText().equals("") && FDPart.getText().equals("")) || (policePart == null && EMSPart == null && FDPart == null)){
+            alert.setContentText("Please enter at least one candidate");
+            alert.show();
+        }else {
+            String headLine = headline.getText();
+            String initUpdate = initialUpdate.getText();
+            String policeName = policePart.getText();
+            String EMSName = EMSPart.getText();
+            String FDName = FDPart.getText();
+            Calendar calendar = Calendar.getInstance();
+            Date publishTime = calendar.getTime();
 
-        // create the data to show in the CheckComboBox
-        final ObservableList<String> strings = FXCollections.observableArrayList();
-        for (int i = 0; i <= 100; i++) {
-            strings.add("Item " + i);
         }
-
-        // Create the CheckComboBox with the data
-        final CheckComboBox<String> checkComboBox = new CheckComboBox<String>(strings);
 
     }
 
--- code/src/main/java/View/orenMain.java
+++ code/src/main/java/View/orenMain.java
@@ -2,6 +2,7 @@ package View;
 
 import Model.Organizations;
 import Model.SQLModel;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,35 +14,35 @@ import java.util.Calendar;
 
 import static javafx.application.Application.launch;
 
-//public class orenMain extends Application{
-////
-////
-////        @Override
-////        public void start(Stage primaryStage) throws Exception{
-////            SQLModel sqlModel = SQLModel.getInstance();
-////            FXMLLoader fxmlControl = new FXMLLoader();
-////            Parent root = fxmlControl.load(getClass().getResource(("OpenWindow.fxml")).openStream());
-////            primaryStage.setTitle("VACATION 4 U ");
-////            OpenWindowController view = fxmlControl.getController();
-////            view.initButtons();
-////            primaryStage.setScene(new Scene(root, 950, 620));
-////            //primaryStage.getScene().getStylesheets().add("/openWindowCss.css");
-////            primaryStage.show();
-////
-////
-////        }
-////
-////        public static void main (String[]args){launch(args);}
-////    }
+public class orenMain extends Application {
 
-public class orenMain {
-    public static void main(String[] args) {
 
-        System.out.println(Organizations.values()[0]);
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            SQLModel sqlModel = SQLModel.getInstance();
+            FXMLLoader fxmlControl = new FXMLLoader();
+            Parent root = fxmlControl.load(getClass().getResource(("CreateEvent.fxml")).openStream());
+            primaryStage.setTitle("VACATION 4 U ");
+            CreateEventController view = fxmlControl.getController();
+            //view.initButtons();
+            primaryStage.setScene(new Scene(root, 950, 620));
+            //primaryStage.getScene().getStylesheets().add("/openWindowCss.css");
+            primaryStage.show();
+
+
+        }
+
+        public static void main (String[]args){launch(args);}
     }
 
+//public class orenMain {
+//    public static void main(String[] args) {
+//
+//        System.out.println(Organizations.values()[0]);
+//    }
 
-}
+
+//}
 
 
 
