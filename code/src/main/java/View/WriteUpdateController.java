907a4fc207f21983e5f2eb16515b0186bf1fb69b
ilayfri <dubhuhvkh1403>
2019-06-14 13:52:04 +0300

    Event almost done

--- code/src/main/java/View/WriteUpdateController.java
+++ code/src/main/java/View/WriteUpdateController.java
@@ -3,31 +3,45 @@ package View;
 import Controllers.CreateController;
 import Controllers.LogedInController;
 import Controllers.SearchController;
+import Model.Event;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 
 public class WriteUpdateController {
     public LogedInController logedInController;
     public CreateController createController;
+    public SearchController searchController;
     public Button sendUpdateButton;
     public ListView events;
     public ObservableList<String> eventsData;
     public javafx.scene.control.TextArea description;
     public String username = "";
     public String updateDescription;
+    String selectedEventName = "";
 
 
     public void SetControllers(LogedInController logedInController, CreateController createController, SearchController searchController){
         this.logedInController = logedInController;
         this.createController = createController;
+        this.searchController = searchController;
         eventsData = searchController.getMyEvents(logedInController.getUserNameFromUserAsStripAndCleanString());
-
         events.setItems(eventsData);
+
+
+        events.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                selectedEventName = events.getSelectionModel().getSelectedItem().toString().split(",")[0];
+            }
+        });
     }
 
     public void sendUpdateAction(ActionEvent actionEvent){
@@ -35,11 +49,12 @@ public class WriteUpdateController {
         if(description.getText().equals("") || description == null) {
             alert.setContentText("Please enter description for this update");
             alert.show();
-
         }// TODO add check for event
         else{
             this.username = logedInController.getUserNameFromUserAsStripAndCleanString();
             this.updateDescription = description.getText();
+
+            createController.createUpdate(this.username,,this.description);
 
         }
     }
