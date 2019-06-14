6a1bb7c9f470454229cca65a7ad8c893c84ad65e
anastasiakova <kovalkov@post.bgu.ac.il>
2019-06-14 10:26:03 +0300

    update constructors

--- code/src/main/java/Model/Update.java
+++ code/src/main/java/Model/Update.java
@@ -1,8 +1,9 @@
 package Model;
 
+import java.sql.PreparedStatement;
 import java.util.Date;
 
-public class Update {
+public class Update implements ISQLable{
     private int id;
     private String description;
     private Date publishDate;
@@ -16,5 +17,35 @@ public class Update {
 
     public Update(String updateRow) {
 
+    }
+
+    @Override
+    public String getPrimaryKey() {
+        return null;
+    }
+
+    @Override
+    public String getPrimaryKeyName() {
+        return null;
+    }
+
+    @Override
+    public void insertRecordToTable(PreparedStatement pstmt) {
+
+    }
+
+    @Override
+    public String getTableFields() {
+        return null;
+    }
+
+    @Override
+    public String getFieldsSQLWithValues() {
+        return null;
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
     }
 }
